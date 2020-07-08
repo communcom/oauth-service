bff4295453931686ef6651a45a46fe3368a04597
Igor Tkachenko <tkachenko.igor88@gmail.com>
2020-07-08 14:04:11 +0300

    Apple token (#11)
    * fix: callback, env

--- src/oauth/index.js
+++ src/oauth/index.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const express = require('express');
+const request = require('request');
 
 const fs = require('fs');
 const path = require('path');
@@ -57,21 +58,13 @@ const oauthProviders = {
         type: 'token',
     },
     apple: {
-        Strategy: require('passport-apple'),
-        requiredEnv: [
-            'APPLE_SERVICE_ID',
-            'APPLE_TEAM_ID',
-            'APPLE_KEY_IDENTIFIER',
-            'APLLE_PRIVATE_KEY',
-        ],
+        Strategy: require('passport-apple-token'),
+        requiredEnv: ['APPLE_CLIENT_ID_WEB', 'APPLE_TEAM_ID', 'APPLE_KEY_ID', 'APLLE_PRIVATE_KEY'],
         options: {
-            clientID: env.APPLE_SERVICE_ID,
+            clientID: env.APPLE_CLIENT_ID_WEB,
             teamID: env.APPLE_TEAM_ID,
-            keyID: env.APPLE_KEY_IDENTIFIER,
-            privateKeyLocation: fs.readFileSync(
-                path.join(__dirname, `../../${env.APLLE_PRIVATE_KEY}`),
-                'utf-8'
-            ),
+            keyID: env.APPLE_KEY_ID,
+            key: fs.readFileSync(path.join(__dirname, `../../${env.APLLE_PRIVATE_KEY}`), 'utf-8'),
             passReqToCallback: true,
         },
         scope: undefined,
@@ -79,16 +72,11 @@ const oauthProviders = {
     },
     'apple-token': {
         Strategy: require('passport-apple-token'),
-        requiredEnv: [
-            'APPLE_SERVICE_ID',
-            'APPLE_TEAM_ID',
-            'APPLE_KEY_IDENTIFIER',
-            'APLLE_PRIVATE_KEY',
-        ],
+        requiredEnv: ['APPLE_CLIENT_ID_APP', 'APPLE_TEAM_ID', 'APPLE_KEY_ID', 'APLLE_PRIVATE_KEY'],
         options: {
-            clientID: env.APPLE_SERVICE_ID,
+            clientID: env.APPLE_CLIENT_ID_APP,
             teamID: env.APPLE_TEAM_ID,
-            keyID: env.APPLE_KEY_IDENTIFIER,
+            keyID: env.APPLE_KEY_ID,
             key: fs.readFileSync(path.join(__dirname, `../../${env.APLLE_PRIVATE_KEY}`), 'utf-8'),
             passReqToCallback: true,
         },
@@ -115,6 +103,10 @@ const buildRoutes = provider => ({
 
 async function strategyCallback(req, accessToken, refreshToken, profile, done) {
     try {
+        if (!profile.provider) {
+            profile.provider = 'apple';
+        }
+
         const rawResult = await createIdentity(profile);
 
         if (rawResult.error) {
@@ -131,6 +123,27 @@ async function strategyCallback(req, accessToken, refreshToken, profile, done) {
 
         return done(err);
     }
+}
+
+function authenticateCallback(req, res) {
+    const { user } = req;
+
+    if (user.code && user.currentState) {
+        res.cookie('commun_oauth_state', user.currentState);
+        res.cookie('commun_oauth_identity', user.identity);
+        res.cookie('commun_oauth_provider', user.provider);
+    }
+
+    if (user.code && user.code === 1101) {
+        res.cookie('commun_oauth_state', 'registered');
+    }
+
+    if (user.success) {
+        res.cookie('commun_oauth_identity', user.identity);
+        res.cookie('commun_oauth_provider', user.provider);
+    }
+
+    return res.redirect('/');
 }
 
 function authenticateTokenCallback(req, res) {
@@ -197,31 +210,20 @@ const oauth = app => {
             );
 
             app.get(route, passport.authenticate(provider, { scope }));
-
-            app.get(
-                callback,
-                passport.authenticate(provider, { failureRedirect: env.FAILURE_REDIRECT_URL }),
-                (req, res) => {
-                    const { user } = req;
-
-                    if (user.code && user.currentState) {
-                        res.cookie('commun_oauth_state', user.currentState);
-                        res.cookie('commun_oauth_identity', user.identity);
-                        res.cookie('commun_oauth_provider', user.provider);
-                    }
-
-                    if (user.code && user.code === 1101) {
-                        res.cookie('commun_oauth_state', 'registered');
-                    }
-
-                    if (user.success) {
-                        res.cookie('commun_oauth_identity', user.identity);
-                        res.cookie('commun_oauth_provider', user.provider);
-                    }
-
-                    return res.redirect('/');
-                }
-            );
+            if (provider === 'apple') {
+                app.post(
+                    callback,
+                    express.urlencoded(),
+                    passport.authenticate(provider),
+                    authenticateCallback
+                );
+            } else {
+                app.get(
+                    callback,
+                    passport.authenticate(provider, { failureRedirect: env.FAILURE_REDIRECT_URL }),
+                    authenticateCallback
+                );
+            }
         }
 
         if (type === 'token') {
@@ -235,12 +237,6 @@ const oauth = app => {
                         },
                         strategyCallback
                     )
-                );
-
-                app.get(
-                    route,
-                    passport.authenticate(provider, { scope }),
-                    authenticateTokenCallback
                 );
 
                 app.post(
@@ -267,6 +263,25 @@ const oauth = app => {
     if (!initializedProviders.length) {
         throw new Error('providers are not initialized');
     }
+
+    app.get('/oauth/apple-token', (req, res) => {
+        request.post(
+            {
+                url: `${env.CALLBACK_AUTH_ROUTE_PREFIX}/oauth/apple-token/callback`,
+                form: {
+                    code: req.query.access_token,
+                },
+            },
+            (err, response, body) => {
+                if (err) {
+                    res.status(401).json({ status: 'false' });
+                }
+
+                res.set('Content-Type', 'application/json');
+                res.send(body);
+            }
+        );
+    });
 
     app.get(env.SUCCESS_REDIRECT_URL, (req, res) => {
         res.json({ status: 'ok' });
